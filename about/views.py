99ba89a8f713e6946b56c7364fcf9ebe5c765030
Nurik4409 <Nurmuhammad2220@icloud.com>
2025-07-28 13:51:34 -0700

    Nurmuhammad 6

--- about/views.py
+++ about/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.viewsets import ViewSet
-from .models import AboutCompanyModel
-from .serializers import AboutCompanySerializer
+from .models import AboutCompanyModel, OurKeysModel
+from .serializers import AboutCompanySerializer, OurKeysSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 
 class HomeViewSet(ViewSet):
     def about_company(self, request, *arg, **kwargs):
-        header = AboutCompanyModel.objects.all().first()
-        serializer = AboutCompanySerializer(header, context={"request": request})
+        about_company = AboutCompanyModel.objects.all().first()
+        serializer = AboutCompanySerializer(about_company, context={"request": request})
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def our_keys(self, request, *arg, **kwargs):
+        our_keys = OurKeysModel.objects.all().first()
+        serializer = OurKeysSerializer(our_keys, context={"request": request})
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
