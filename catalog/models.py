27d7afca3db7991ad97e50a0bf3c13afda365f45
yusuf <valixonovyusuf01@gmail.com>
2025-07-28 16:41:10 +0500

    added lang

--- catalog/models.py
+++ catalog/models.py
@@ -1,26 +1,32 @@
 from django.db import models
-
+from parler.models import TranslatableModel, TranslatedFields
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=255)
+    )
 
     def __str__(self):
         return self.name
 
-class Product(models.Model):
+class Product(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=255),
+        description = models.TextField()
+    )
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
     image = models.ImageField(upload_to='products/')
 
     def __str__(self):
         return self.name
     
-class Material(models.Model):
+class Material(TranslatableModel):
+    translations = TranslatedFields(
+        name = models.CharField(max_length=255),
+        description = models.TextField()
+    )
     category = models.ForeignKey(Category, related_name='materials', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
     image = models.ImageField(upload_to='materials/')
 
     def __str__(self):
